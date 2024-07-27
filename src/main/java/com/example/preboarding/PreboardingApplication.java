87adeae3309fda92fc98c7d5bd2c595787f3759d
Minerva08 <elavl@naver.com>
2024-07-27 15:35:44 +0900

    first commit

---
+++ src/main/java/com/example/preboarding/PreboardingApplication.java
@@ -0,0 +1,13 @@
+package com.example.preboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreboardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/preboarding/PreboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
