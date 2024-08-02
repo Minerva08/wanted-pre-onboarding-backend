8d89e31d1068ad2caa86607082a7868962ce3193
Minerva08 <elavl@naver.com>
2024-08-02 23:05:36 +0900

    QueryDsl 적용

---
+++ src/main/java/com/example/preboarding/config/QueryDslConfig.java
@@ -0,0 +1,22 @@
+package com.example.preboarding.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+@Configuration
+@EnableJpaRepositories("com.example.preboarding.repository")
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
