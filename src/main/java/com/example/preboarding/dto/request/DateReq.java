48fcac3972f42180b0702bbaa2c16ee47cbb5834
Minerva08 <elavl@naver.com>
2024-08-13 02:28:42 +0900

    채용 공고 수정일자 @Nullable 추가

--- src/main/java/com/example/preboarding/dto/request/DateReq.java
+++ src/main/java/com/example/preboarding/dto/request/DateReq.java
@@ -1,12 +1,14 @@
 package com.example.preboarding.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
 public class DateReq {
     @Schema(example = "2024-08-03 11:00:00", description="채용 공고 수정일")
+    @Nullable
     private String updateDate;
     @NotEmpty(message = "Required")
     @Schema(example = "2024-08-01 29:00:00", description="채용 시작일")
